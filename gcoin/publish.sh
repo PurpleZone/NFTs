d890add124d6a998ffba6bc6e68a5a125e259e8b
Shannon J. Bosowski <sbosowski@happycoin.gq>
2022-05-03 21:21:56 +0200

    updated on 22-05-03

--- gcoin/publish.sh
+++ gcoin/publish.sh
@@ -6,11 +6,14 @@ ppath=$(ipfs name resolve $mutkey)
 qmprev=${ppath#/ipfs/}
 echo prev: $qmprev
 
+find . -name '*~1' -delete
+pandoc -t html -o factsheet.html factsheet.md
 pandoc -t html -o README.html README.md
 qm=$(ipfs add -w README.md gcoin.* *.pdf *.htm* -Q)
-echo url: https://nftstorage.link/ipfs/$qm
+echo url: https://ipfs.safewatch.xyz/ipfs/$qm
 
-ipfs name publish --key=$symb $qm --allow-offline
+ipfs name publish --key=$symb $qm --ipns-base b58mh --allow-offline
 ipfs pin remote add --service=nft --name=$symb --background $qm
+echo url: https://nftstorage.link/ipfs/$qm
 exit $?
 
