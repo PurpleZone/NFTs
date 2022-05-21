0728aa10ea188538d1783ae15775b76491ca0033
Shannon J. Bosowski <sbosowski@happycoin.gq>
2022-05-21 10:34:07 +0200

    price on 2022-05-21 @ 10:34: 578.05404436129981558260 SOL

--- prices/priceBTC.sh
+++ prices/priceBTC.sh
@@ -11,9 +11,10 @@ chf=eXMH_Xz9YUhh
 eur=5k-_VTxqtCEI
 api_url=https://api.coinranking.com/v2
 
-#find . -name 'coinset.json' -mtime 1 -delete
-find . -maxdepth 1 -name 'coinset.json' -mtime +1 -delete
-if [ ! -e coinset.json ]; then
+find . -maxdepth 1 -name 'coinset.json' -mtime +1
+stat coinset.json
+found=$(find . -name 'coinset.json' -mtime +0 | wc -l)
+if [ "$found" != '1' ]; then
 tic=$(date +%s%N | cut -c-13)
 priceSOLinCHF=$(curl -s $api_url/coin/$sol/price?referenceCurrencyUuid=$chf | json_xs -t string -e '$_ = $_->{data}{price}')
 echo SOL price: $priceSOLinCHF CHF
